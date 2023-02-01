3968c6b0e1ec476a424fa8ca20bdb21059e7add0
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2023-02-01 18:36:24 +0500

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './VendingMachine.json';
 
 export const contractAbi = abi.abi;
-export const contractAddress = "0x150C4D345Fd0B1F474458C0348fB53ca17259020";
+export const contractAddress = "0x5f92a4cD7cbF3CF2fd6801c2EF8902FF24431368";
