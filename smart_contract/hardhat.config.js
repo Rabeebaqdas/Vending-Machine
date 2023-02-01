09a82c926426051e0bed5db8e0409d86170c7358
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2023-02-01 18:36:58 +0500

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,14 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+
+
+require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten : {
-      url : "https://ropsten.infura.io/v3/907414f040764de49665bf288d2b75cd",
-      accounts : ['5891b493c71e777c3d64d82695e379e4738600dfcbce1dc272c6c08e3f094c35']
+  solidity : '0.8.4', // solidity version of smart contract
+  networks : {
+    goerli : {
+      url :"https://eth-goerli.g.alchemy.com/v2/ALCHEMY_API_KEY", // alchemy url for the deployment of smart contract
+      accounts : [''] //private key
     }
   }
-};
-
-
-//0x907464c849893a602530d2727883474ab4110d55
+}
